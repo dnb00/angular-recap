16d898910824933ae0e1f307673864dee2aa7d22
dnb00 <danielbatista11@outlook.com>
2024-02-26 21:03:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-recap
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/dnb00/angular-recap)
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  providers: [HousingService],
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
+
+  constructor(private readonly housingService: HousingService) {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
